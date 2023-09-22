37b227b32b916f35f5607710d26dba16065e1043
mchacher <marc.chachereau@gmail.com>
2023-09-22 14:58:41 +0200

    add comments and flush method

--- src/queue.h
+++ src/queue.h
@@ -8,17 +8,59 @@
 template <typename T, int MAX_QUEUE_SIZE>
 class Queue {
 public:
+    /**
+     * @brief Constructs a new Queue instance.
+     */
     Queue();
 
+    /**
+     * @brief Initializes the queue by resetting indices.
+     */
     void setup();
+
+    /**
+     * @brief Checks if the queue is empty.
+     * 
+     * @return true if the queue is empty, false otherwise.
+     */
     bool isEmpty();
+
+    /**
+     * @brief Checks if the queue is full.
+     * 
+     * @return true if the queue is full, false otherwise.
+     */
     bool isFull();
+
+    /**
+     * @brief Pushes an item onto the queue.
+     * 
+     * @param item The item to push onto the queue.
+     * @return true if the item was successfully pushed, false if the queue is full.
+     */
     bool push(const T &item);
+
+    /**
+     * @brief Pops (removes and retrieves) an item from the queue.
+     * 
+     * @param item The item to pop from the queue.
+     * @return true if an item was successfully popped, false if the queue is empty.
+     */
     bool pop(T &item);
 
-    // Accessor method to get the front item without removing it
+    /**
+     * @brief Gets the front item in the queue without removing it.
+     * 
+     * @param item The item to store the front item.
+     * @return true if the front item was successfully retrieved, false if the queue is empty.
+     */
     bool frontItem(T &item);
 
+    /**
+     * @brief Removes all items from the queue.
+     */
+    void flush();
+    
 private:
     T queue[MAX_QUEUE_SIZE];
     int8_t front;
@@ -82,5 +124,11 @@ bool Queue<T, MAX_QUEUE_SIZE>::frontItem(T &item) {
     }
 }
 
+template <typename T, int MAX_QUEUE_SIZE>
+void Queue<T, MAX_QUEUE_SIZE>::flush() {
+    // Reset front and rear to empty the queue
+    front = 0;
+    rear = -1;
+}
 
 #endif  // QUEUE_H
