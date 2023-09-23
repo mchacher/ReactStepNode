52d9d3b6ec99cfe57b207e65372a2010b82964f5
mchacher <marc.chachereau@gmail.com>
2023-09-23 15:48:23 +0200

    clean up events

--- src/event_type.h
+++ src/event_type.h
@@ -6,15 +6,19 @@
 
 // Define an enumeration to represent system event types with hex values
 typedef enum {
-    EVENT_SYS_TYPE_START = 0x10,
-    EVENT_SYS_TYPE_STOP = 0x12,
-    EVENT_SYS_TYPE_PAUSE = 0x13,
+    // System Events
+    EVENT_SYS_TYPE_START    = 0x10,
+    EVENT_SYS_TYPE_STOP     = 0x12,
+    EVENT_SYS_TYPE_PAUSE    = 0x13,
+    EVENT_SYS_TYPE_SET      = 0x14,
     // Add more system event types as needed with appropriate hex values
     // ...
-    EVENT_SYS_TYPE_RESET = 0x3F,
-    EVENT_APP_TYPE_FOOT_PRESS = 0x40,
-    EVENT_APP_TYPE_FOOT_RIGHT_PRESS = 0x41,
-    EVENT_APP_TYPE_FOOT_LEFT_PRESS = 0x42,
+    EVENT_SYS_TYPE_RESET    = 0x3F,
+    
+    // Application Events
+    EVENT_APP_TYPE_FOOT_PRESS           = 0x40,
+    EVENT_APP_TYPE_FOOT_PRESS_RIGHT     = 0x41,
+    EVENT_APP_TYPE_FOOT_PRESS_LEFT      = 0x42,
     // Add more workout event types as needed with appropriate hex values
     // ...
 } EVENT_TYPE;
