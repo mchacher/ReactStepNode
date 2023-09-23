c5b65914e77461fd8ad99d7c2e893ff83d626811
mchacher <marc.chachereau@gmail.com>
2023-09-23 15:49:27 +0200

    one single hardware config file

---
+++ src/hardware_config.h
@@ -0,0 +1,25 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define MINI_STEP_MOCK_UP 1
+
+// TM 1637 display
+#define PIN_TM_1637_CLK     11
+#define PIN_TM_1637_DIO     12
+
+// Foot Sensor
+#define PIN_PB_FOOT_LEFT    7
+#define PIN_PB_FOOT_RIGHT   6
+
+// push buttons
+#define PIN_PB_SET          5
+#define PIN_PB_STOP         9
+#define PIN_PB_CUSTOM       8
+#define PIN_PB_PLAY_PAUSE   10
+
+// #leds are in the strip
+#define PIN_LED_DATA        4
+#define NUM_LEDS            3
+
+
+#endif
