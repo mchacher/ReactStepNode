1ef45848929b8332c28a8a16ac39647f19c8a08c
mchacher <marc.chachereau@gmail.com>
2023-09-25 07:36:13 +0200

    new driver

--- src/display.cpp
+++ src/display.cpp
@@ -1,22 +1,65 @@
 #include "Arduino.h"
 #include "display.h"
-#include <TM1637.h>
+#include <TM1637TinyDisplay.h>
 #include <ArduinoLog.h>
 #include "hardware_config.h"
+#include "state_machine.h"
 
-// Instantiation and pins configurations
-TM1637 tm(PIN_TM_1637_CLK, PIN_TM_1637_DIO);
+TM1637TinyDisplay display(PIN_TM_1637_CLK, PIN_TM_1637_DIO);
+
+
+static const char *msg = MSG_HELLO;
+static unsigned long expiration_time = 0;
 
 void display_setup()
 {
     Log.noticeln(F("display: setup"));
-    tm.begin();
-    tm.setBrightness(5);
-    tm.display("HELLO");
+    display.begin();
+    display.setBrightness(5);
+
+}
+
+void display_message(const char *message)
+{
+    
+    display.stopAnimation();
+    display.clear();
+    msg = message;
+    display.startStringScroll_P(msg, 300);
+    expiration_time = 0; // msg will be displayed indefinitely
+}
+
+
+void display_message(const char *message, uint8_t duration)
+{
+    
+    display.stopAnimation();
+    display.clear();
+    msg = message;
+    display.startStringScroll_P(msg, 300);
+    expiration_time = millis() + duration * 1000;
 }
 
 
 void display_task()
 {
-    // Nothing
+    // display.showString(msg);
+    bool isAnimationRunning = display.Animate();
+    
+    if (!isAnimationRunning)
+    {
+        Log.verbose(F("display: Animation not running"));
+        display.startStringScroll_P(msg, 300);
+    }
+
+    if (expiration_time != 0 && millis() > expiration_time)
+    {
+        display_clear();
+    }
+}
+
+void display_clear()
+{
+    display.clear();
+    msg= MSG_NONE;
 }
--- src/display.h
+++ src/display.h
@@ -3,7 +3,20 @@
 
 #include "Arduino.h"
 
+const PROGMEM char MSG_NONE[] = "";
+const PROGMEM char MSG_HELLO[] = "HELLO";
+const PROGMEM char MSG_READY[] = "READY";
+const PROGMEM char MSG_STOP[] = "STOP";
+const PROGMEM char MSG_PAUSE[] = "PAUSE";
+const PROGMEM char MSG_GO[] = " GO ";
+const PROGMEM char MSG_ERROR[] = "ERROR";
+const PROGMEM char MSG_SET[] = "SET";
+const PROGMEM char MSG_IDLE[] = "IDLE";
+
 void display_setup();
 void display_task();
+void display_message(const char *message);
+void display_message(const char *message, uint8_t duration);
+void display_clear();
 
 #endif
