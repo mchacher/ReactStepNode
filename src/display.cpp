cd4331009ddc3e5f0143749fb467fe71ba616fc7
mchacher <marc.chachereau@gmail.com>
2023-09-26 22:48:41 +0200

    add blink for number, and queued messages

--- src/display.cpp
+++ src/display.cpp
@@ -4,12 +4,21 @@
 #include <ArduinoLog.h>
 #include "hardware_config.h"
 #include "state_machine.h"
+#include "queue.h"
 
 TM1637TinyDisplay display(PIN_TM_1637_CLK, PIN_TM_1637_DIO);
 
 static const char *msg = MSG_HELLO;
+static int number;
 static unsigned long expiration_time = 0;
 static bool task_active = false;
+static bool blink = false;
+static bool show_number = true;
+
+// Define the maximum queue size
+const int MAX_QUEUE_SIZE = 10;
+// Create a Queue instance for Messages
+Queue<QueuedMessage, MAX_QUEUE_SIZE> messages_queue;
 
 /**
  * @brief Initialize the display and set its brightness.
@@ -54,8 +63,25 @@ void display_message(const char *message, uint8_t duration)
     display.clear();
     msg = message;
     display.startStringScroll_P(msg, 300);
-    expiration_time = millis() + duration * 1000;
+    if (duration == 0)
+    {
+        expiration_time = 0;
+    }
+    else
+    {
+        expiration_time = millis() + duration * 1000;
+    }
+
     task_active = true;
+}
+
+void display_push_message_to_queue(const char *message, uint8_t duration)
+{
+    QueuedMessage qmsg;
+    Log.noticeln(F("display: pushing message to queue"));
+    qmsg.msg = message;
+    qmsg.duration = duration;
+    messages_queue.push(qmsg);
 }
 
 /**
@@ -68,13 +94,27 @@ void display_number(uint16_t num)
 {
     if (task_active)
     {
-
         display.stopAnimation();
         display.clear();
         msg = MSG_NONE;
         task_active = false;
     }
-    display.showNumber((int)num, true);
+    number = (int)num;
+    show_number = true;
+    if (blink == false)
+    {
+        display.showNumber(number, true);
+    }
+}
+
+/**
+ * @brief Enable / Disable blinking of display (only for numbers)
+ *
+ * @param state if True
+ */
+void display_blink_numbers(bool state)
+{
+    blink = state;
 }
 
 /**
@@ -82,19 +122,41 @@ void display_number(uint16_t num)
  */
 void display_task()
 {
+    static unsigned long last_blink = 0;
+    unsigned long tick = millis();
+    static bool on_state = true;
+
     if (task_active)
     {
         bool isAnimationRunning = display.Animate();
-
         if (!isAnimationRunning)
         {
             Log.verbose(F("display: Animation not running"));
             display.startStringScroll_P(msg, 300);
+            if (expiration_time != 0 && millis() > expiration_time)
+            {
+                QueuedMessage qmsg;
+                if (messages_queue.pop(qmsg))
+                {
+                    Log.noticeln(F("display: pop message from queue"));
+                    display_message(qmsg.msg, qmsg.duration);
+                }
+            }
         }
-
-        if (expiration_time != 0 && millis() > expiration_time)
+    }
+    else if ((blink == true) && (show_number == true))
+    {
+        if ((tick - last_blink) > 700 && (on_state))
         {
             display_clear();
+            on_state = false;
+            last_blink = millis();
+        }
+        else if ((tick - last_blink) > 300 && (!on_state))
+        {
+            display.showNumber(number, true);
+            on_state = true;
+            last_blink = millis();
         }
     }
 }
--- src/display.h
+++ src/display.h
@@ -11,7 +11,13 @@ const PROGMEM char MSG_PAUSE[] = "PAUSE";
 const PROGMEM char MSG_GO[] = " GO ";
 const PROGMEM char MSG_ERROR[] = "ERROR";
 const PROGMEM char MSG_SET[] = "SET";
-const PROGMEM char MSG_IDLE[] = "IDLE";
+const PROGMEM char MSG_IDLE[] = "----";
+
+struct QueuedMessage
+{
+    const char* msg;
+    uint8_t duration;
+};
 
 void display_setup();
 void display_task();
@@ -19,5 +25,7 @@ void display_message(const char *message);
 void display_message(const char *message, uint8_t duration);
 void display_number(uint16_t num);
 void display_clear();
+void display_push_message_to_queue(const char *message, uint8_t duration);
+void display_blink_numbers(bool state);
 
 #endif
