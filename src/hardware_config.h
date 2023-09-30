a6196acdff450f204bf43b36f0c3794194eee05c
mchacher <marc.chachereau@gmail.com>
2023-09-30 08:39:13 +0200

    new pin assignment

--- src/hardware_config.h
+++ src/hardware_config.h
@@ -4,17 +4,17 @@
 #define MINI_STEP_MOCK_UP 1
 
 // TM 1637 display
-#define PIN_TM_1637_CLK     11
-#define PIN_TM_1637_DIO     12
+#define PIN_TM_1637_CLK     2
+#define PIN_TM_1637_DIO     3
 
 // Foot Sensor
-#define PIN_PB_FOOT_LEFT    3
-#define PIN_PB_FOOT_RIGHT   6
+#define PIN_PB_FOOT_LEFT    14
+#define PIN_PB_FOOT_RIGHT   15
 
 // push buttons
 #define PIN_PB_SET          5
 #define PIN_PB_STOP         9
-#define PIN_PB_CUSTOM       2
+#define PIN_PB_CUSTOM       6
 #define PIN_PB_PLAY_PAUSE   10
 
 // #leds are in the strip
