ef03182d98aacfdc9003316c847cb814169f21be
mchacher <marc.chachereau@gmail.com>
2023-10-06 18:49:57 +0200

    major refactor with support for RP 2040

--- src/hardware_config.h
+++ src/hardware_config.h
@@ -2,7 +2,7 @@
 #define CONFIG_H
 
 #define REACT_MESH              1 // set to 1 to activate RADIO
-#define LOCAL_COMMAND_BUTTONS   1 // set to 1 to activate PLAY/PAUSE, STOP and CUSTOM Buttons
+m // set to 1 to activate PLAY/PAUSE, STOP and CUSTOM Buttons
 #define DIGITAL_FOOT_SENSOR     1 // set to 1 for digital foot sensor (typically on mock up)
 
 // TM 1637 display
